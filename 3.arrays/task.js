bfbf9f2fec0453b23fafe4ced8c9f33e7187210a
Neto test <ryo45wantage@gmail.com>
2026-09-21 16:19:11 +0500

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,27 @@
+/**
+ * Задача 1. Сравнение массивов
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @returns {boolean}
+ */
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
+/**
+ * Задача 2. Средний возраст пользователей одного пола
+ * @param {Array} users
+ * @param {string} gender
+ * @returns {number}
+ */
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filteredUsers = users.filter(user => user.gender === gender);
+
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+
+  return filteredUsers
+    .map(user => user.age)
+    .reduce((sum, age) => sum + age, 0) / filteredUsers.length;
 }
