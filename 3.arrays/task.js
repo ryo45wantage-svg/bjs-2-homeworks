68e27beb0e6214c63e1806ead9fa5a1d357e0cde
Neto test <ryo45wantage@gmail.com>
2026-09-21 16:26:09 +0500

    Исправлен task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,19 +1,7 @@
-/**
- * Задача 1. Сравнение массивов
- * @param {Array} arr1
- * @param {Array} arr2
- * @returns {boolean}
- */
 function compareArrays(arr1, arr2) {
   return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
-/**
- * Задача 2. Средний возраст пользователей одного пола
- * @param {Array} users
- * @param {string} gender
- * @returns {number}
- */
 function getUsersNamesInAgeRange(users, gender) {
   const filteredUsers = users.filter(user => user.gender === gender);
 
